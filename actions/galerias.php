85f46e3490c7fc323b3d29ed4d4376f1b0a2c44d
renej <rponce@scimaginsoft.com>
2014-05-19 22:25:26 -0500

    Action para galeria

---
+++ actions/galerias.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rene
+ * Date: 19/05/14
+ * Time: 09:52 PM
+ */
+
+require '../class/Galerias.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$galeria = new Galerias();
+
+$id_colegio         = $_POST['id_colegio'];
+$fecha              = $_POST['fecha'];
+$ruta               = '/ruta/a/mi/archivo/';
+$nombre             = $_POST['nombre'];
+$descripcion        = $_POST['descripcion'];
+$id_usuario         = $_POST['id_usuario'];
+$accion             = $_POST['accion'];
+
+$params = array(
+    'id_colegio'    => $id_colegio,
+    'fecha'         => $fecha,
+    'ruta'          => $ruta,
+    'nombre'        => $nombre,
+    'descripcion'   => $descripcion,
+    'id_usuario'    => $id_usuario
+);
+
+if($accion == 'agregar') {
+    var_dump($params);
+    $galeria->add($params);
+}
