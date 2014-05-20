21c5c5968a5338f2ed1eaec4735ad7f60cf5106a
renej <rponce@scimaginsoft.com>
2014-05-19 21:50:19 -0500

    Cupones modificacion

--- class/Cupones.php
+++ class/Cupones.php
@@ -21,7 +21,7 @@ class Cupones extends CrudModel {
         }
     }
 
-    //inserta un nuevo usuario
+    //inserta un nuevo cupon
     public function add($cupon = array()) {
         //consulta parametrizada
         $this->query = 'INSERT INTO cupones(descuento, fecha_inicio, fecha_fin, id_usuario, tipo_usuario, cupon, estatus)
@@ -30,14 +30,14 @@ class Cupones extends CrudModel {
         $this->dynamic_query($cupon, true);
     }
 
-    //actualiza en este caso un usuario, pero puede ser cualquier otra cosa
+    //actualiza en este caso un cupon, pero puede ser cualquier otra cosa
     public function edit($cupon = array()) {
         $this->query = 'UPDATE cupones SET descuento = :descuento, fecha_inicio = :fecha_inicio, fecha_fin = :fecha_fin,
         id_usuario = :id_usuario, tipo_usuario = :tipo_usuario, cupon = :cupon, estatus = :estatus WHERE id = :id';
         $this->dynamic_query($user, true);
     }
 
-    //elimina un usuario por su id
+    //elimina un cupon por su id
     public function delete($id = array()) {
         $this->query = 'DELETE FROM cupones WHERE id = :id';
         $this->dynamic_query($id, true);
