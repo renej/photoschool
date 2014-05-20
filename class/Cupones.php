a762fee062b2e6df1d566f541f2b5bc8a663923b
renej <rponce@scimaginsoft.com>
2014-05-19 21:37:53 -0500

    Modificacion clase cupones

--- class/Cupones.php
+++ class/Cupones.php
@@ -10,29 +10,36 @@ require 'CrudModel.php';
 class Cupones extends CrudModel {
 
     public function __construct() {
-        $this->_dbName = 'photoschool';
+        $this->_dbName = "photoschool";
     }
 
     public function get() {
-        $this->query = "SELECT * FROM cupones";
+        $this->query = 'SELECT * FROM cupones';
         $this->simple_query();
+        if($this->rows > 0){
+            return $this->rows;
+        }
     }
 
+    //inserta un nuevo usuario
     public function add($cupon = array()) {
-        $this->query = "INSERT INTO cupones (descuento, fecha_inicio, fecha_fin, id_usuario, tipo_usuario, cupon, estatus)
-        VALUES (:descuento, :fecha_inicio, :fecha_fin, :id_usuario, :tipo_usuario, :cupon, :estatus)";
+        //consulta parametrizada
+        $this->query = 'INSERT INTO cupones(descuento, fecha_inicio, fecha_fin, id_usuario, tipo_usuario, cupon, estatus)
+        VALUES(:descuento, :fecha_inicio, :fecha_fin, :id_usuario, :tipo_usuario, :cupon, :estatus)';
+        //con el segundo parámetro a true hacemos un insert
         $this->dynamic_query($cupon, true);
     }
 
+    //actualiza en este caso un usuario, pero puede ser cualquier otra cosa
     public function edit($cupon = array()) {
-        $this->query = "UPDATE cupones SET descuento = :descuento, fecha_inicio = :fecha_inicio, fehca_fin = :fecha_fin,
-        id_usuario = :id_usuario, tipo_usuario = :tipo_usuario, cupo = :cupon, estatus = :estatus WHERE id = :id";
-        $this->dynamic_query($cupon, true);
+        $this->query = 'UPDATE cupones SET descuento = :descuento, fecha_inicio = :fecha_inicio, fecha_fin = :fecha_fin,
+        id_usuario = :id_usuario, tipo_usuario = :tipo_usuario, cupon = :cupon, estatus = :estatus WHERE id = :id';
+        $this->dynamic_query($user, true);
     }
 
-    public function delete($cupon = array()) {
-        $this->query = "DELETE FROM cupones WHERE id = :id";
-        $this->dynamic_query($cupon, true);
+    //elimina un usuario por su id
+    public function delete($id = array()) {
+        $this->query = 'DELETE FROM cupones WHERE id = :id';
+        $this->dynamic_query($id, true);
     }
-
 } 
